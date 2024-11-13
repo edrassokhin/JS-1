c690485155087d9f2f0c2c02cf24c56023ee747d
Ed <edward.rassohin@gmail.com>
2024-11-13 20:58:58 +0500

    Добавлены файлы с первого урока

---
+++ script.js
@@ -0,0 +1,72 @@
+// Вывод информации в консоль
+// console.log("Hello, World!");
+// Окно с сообщением
+// alert("Метод alert");
+// Диалоговое окно
+// nameUser = prompt("Как вас зовут?", "Нет данных");
+// alert(nameUser)
+// Выбор да или нет
+// confirm("Подтвердите действие");
+// Строки
+// let str1 = "str1";
+// let str2 = 'str2';
+
+// Вывод информации
+// 1 способ - обратные кавычки
+// let name = "Ed";
+// let info = `Hello, ${name}`;
+// console.log(info);
+
+// 2 способ - конкатенация строк
+// let val1 = "Привет, я ";
+// let val2 = "Эдуард";
+// let res = val1 + val2;
+// console.log(res);
+
+// Длина строки в символах
+// console.log(val1.length);
+// 1 способ
+// console.log("Длина строки " + val1 + " = " + val1.length)
+// 2 способ
+// let data = `Длина строки ${val1} = ${val1.length}`;
+// console.log(data);
+
+// Регистр
+// let str = "Hello";
+// console.log(str.toUpperCase());
+
+// let res = str.toLowerCase();
+// console.log(res);
+
+// True or False
+// let info = "JavaScript";
+// console.log(info.includes("Java"));
+// console.log(info.includes("Python"));
+
+// Тип данных
+// console.log(typeof 8);
+// console.log(typeof "str");
+// console.log(typeof true);
+
+// Преобразование типов данных
+// let value = 11;
+// console.log(String(value));
+
+// 1
+// let value2 = "5";
+// console.log(Number(value2))
+// 2
+// let num = Number(value2);
+// console.log(typeof num);
+// 3
+// let num2 = +value2;
+// console.log(typeof num2);
+
+// let str = "";
+// console.log(Boolean(str));
+
+// let str1 = "Hello";
+// console.log(Boolean(str1));
+
+// let num = 0;
+// console.log(Boolean(num));
